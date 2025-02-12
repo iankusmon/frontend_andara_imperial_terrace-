246dae21f9d8ef9d6f433b7bb7a4b0d7726643b5
iankusmon <muh.rahardian.k@gmail.com>
2025-02-13 02:35:15 +0700

    fix: port for backend

--- app/simulasi-pembayaran-kpr/page.tsx
+++ app/simulasi-pembayaran-kpr/page.tsx
@@ -6,7 +6,7 @@ export default function KprCorner() {
     <>
       <SimulasiPembayaranKpr
       title={'Simulasi Pembayaran KPR'}
-      endpoint={' http://127.0.0.1:3099/api/customers/kpr_calculators'}
+      endpoint={' http://127.0.0.1:3000/api/customers/kpr_calculators'}
       />
     </>
   
