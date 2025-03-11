af1128375b172e246ddfa5c101baabdcadd8b209
iankusmon <muh.rahardian.k@gmail.com>
2025-03-11 15:49:49 +0700

    chore: add VideoLanding to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
-import Hero from '@/components/Hero'
+"use client";
+
+import { useState, useEffect, useRef } from 'react';
+
+import Hero from '@/components/Hero';
+import Navbar from '@/components/Navbar';
+import VideoLanding from '@/components/VideoLanding';
 import DayClub from '../components/DayClub'; // Komponen Day Club
 import MiniHomepage from '@/components/MiniHomepage';
 import VillaSlider from '@/components/VillaSlider';
@@ -6,7 +12,7 @@ import AlurPemesanan from '@/components/AlurPemesanan';
 import AffiliateBanner from '@/components/AffiliateBanner';
 import FreshChat from "@/components/FreshChat";
 
-// pages/_app.tsx
+// Impor CSS eksternal
 import 'leaflet/dist/leaflet.css';  // Impor CSS Leaflet
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -20,18 +26,33 @@ const PopupBanner = dynamic(() => import("@/components/PopupBanner"), {
 const DynamicMap = dynamic(() => import('../components/Map'), { ssr: false });
 
 export default function Home() {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > window.innerHeight * 0.8) {
+        setShowNavbar(true);
+      } else {
+        setShowNavbar(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <>
       <PopupBanner />
+      <VideoLanding />
       <Hero />
       <VillaSlider />
       <DynamicMap />
-      <DayClub/>
+      <DayClub />
       <MiniHomepage />
-      <AlurPemesanan/>
-      <AffiliateBanner/>
+      <AlurPemesanan />
+      <AffiliateBanner />
       <FreshChat />
     </>
-  
-  )
+  );
 }
