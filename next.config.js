d14f96a8051f7a54a8678b56a558f1b6f0ce218b
iankusmon <muh.rahardian.k@gmail.com>
2025-03-24 00:02:01 +0700

    config: add timed out setting pm2 setting

--- next.config.js
+++ next.config.js
@@ -3,5 +3,17 @@ module.exports = {
   images: {
     unoptimized: true,
   },
+  apps: [
+    {
+      name: "next-app",
+      script: "npm",
+      args: "start",
+      env: {
+        NODE_ENV: "production",
+        NEXT_PUBLIC_API_TIMEOUT: "120000", // Tambahkan timeout sebagai env variable
+      },
+      max_memory_restart: "1G",
+    },
+  ],
   // other configurations...
 };
